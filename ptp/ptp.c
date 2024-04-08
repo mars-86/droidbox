13e0982cca68bb11f90595679c9d7e7605fced6e
mars-86 <hrslash86@gmail.com>
2024-04-07 21:41:49 -0300

    feat: ptp_get_device_prop_desc implementation

--- ptp/ptp.c
+++ ptp/ptp.c
@@ -501,3 +501,22 @@ int ptp_power_down(ptp_dev_t* dev, ptp_res_t* res)
 
     return __handle_request(dev->fd, dev->endp, &__ptpcmd, NULL, 0, res, NULL, RESPONSE_PHASE);
 }
+
+int ptp_get_device_prop_desc(ptp_dev_t* dev, uint32_t device_prop_code, uint8_t* data, uint32_t len, ptp_res_t* res)
+{
+    int nparams = 1;
+    struct ptp_cmd_container __ptpcmd = { 0 };
+
+    __ptpcmd.header.ContaierLength = PTP_REQUEST_LEN(nparams);
+    __ptpcmd.header.ContainerType = PTP_CONTAINER_TYPE_COMMAND_BLOCK;
+    __ptpcmd.header.Code = PTP_REQUEST_GET_DEVICE_PROP_DESC;
+    __ptpcmd.header.TransacionID = __get_transaction_id();
+
+    __ptpcmd.payload.Parameter1 = device_prop_code;
+
+#ifdef __DEBUG
+    printf("GET DEVICE PROP DESC\n");
+#endif
+
+    return __handle_request(dev->fd, dev->endp, &__ptpcmd, data, len, res, NULL, RESPONSE_PHASE);
+}
